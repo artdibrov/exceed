c21d415f6f76fbd44dd6a3952d14c1862eb99b97
Arthur <artdibrov@proton.me>
2026-07-23 16:50:18 +0300

    Create renderer.py

---
+++ renderer.py
@@ -0,0 +1,15 @@
+class Renderer:
+
+    def show(self, grid):
+
+        print()
+
+        for row in grid:
+
+            print(
+
+                "".join(row)
+
+            )
+
+        print()
